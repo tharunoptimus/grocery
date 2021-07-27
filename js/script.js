5a78bac4a03ca7801dd575ff96ba0ca97c546375
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-07-28 03:53:16 +0530

    PWA + Offline

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+if(window.location.hostname !== 'localhost') {
+    if (location.protocol !== 'https:') {
+        location.replace(`https:${location.href.substring(location.protocol.length)}`);
+    }
+}
+
 let schemaBuilder = lf.schema.create("todo", 1);
 schemaBuilder
 	.createTable("Item")
@@ -12,7 +18,7 @@ let item;
 schemaBuilder.connect().then(function (db) {
 	todoDb = db;
 	item = db.getSchema().table("Item");
-    showResults();
+	showResults();
 });
 
 function createItem(product) {
@@ -20,17 +26,20 @@ function createItem(product) {
 		product: product,
 		done: false,
 	});
-    let content = {
-        id: 0,
-        product: product,
-        done: false
-    }
-	todoDb.insertOrReplace().into(item).values([row]).exec().then(
-        function(rows) {
-            content.id = rows[0].id;
-        });
-    return content;
-    
+	let content = {
+		id: 0,
+		product: product,
+		done: false,
+	};
+	todoDb
+		.insertOrReplace()
+		.into(item)
+		.values([row])
+		.exec()
+		.then(function (rows) {
+			content.id = rows[0].id;
+		});
+	return content;
 }
 
 async function updateItem(id, value) {
@@ -51,27 +60,28 @@ function showResults() {
 		.from(item)
 		.exec()
 		.then(function (results) {
-			showItems(results)
+			showItems(results);
 		});
 }
 
-function showItems (lists) {
-    let html = "";
-    lists.forEach(list => {
-        html = createHtml(list.id, list.product, list.done);
-        $(".listContainer").prepend(html);
-    });
+function showItems(lists) {
+	let html = "";
+	lists.forEach((list) => {
+		html = createHtml(list.id, list.product, list.done);
+		$(".listContainer").prepend(html);
+	});
 }
 
 function createHtml(id, product, done) {
-
 	let className = done ? "done" : "";
-    let justifyContent = done ? "style='justify-content: flex-start' " : "";
-    let tick = done ? "" : `<span class="iconHolder tickIcon">
+	let justifyContent = done ? "style='justify-content: flex-start' " : "";
+	let tick = done
+		? ""
+		: `<span class="iconHolder tickIcon">
             <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="check-circle" class="svg-inline--fa fa-check-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" >             
                 <path fill="currentColor" d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"></path>
             </svg>
-        </span>`
+        </span>`;
 
 	return `<div class="listItem ${className}" data-id="${id}">
     ${tick}
@@ -84,27 +94,33 @@ function createHtml(id, product, done) {
     </div>`;
 }
 
+$("span.refreshIcon").click(function () {
+	window.location.reload();
+});
+
 $("span.plusIcon").click(function () {
 	let product = prompt("Enter a Product Name: ");
 	if (product == null || product == "") {
 		return;
 	}
 	let content = createItem(product);
-    let html = createHtml(content.id, content.product, content.done);
-    $(".listContainer").prepend(html);
+	let html = createHtml(content.id, content.product, content.done);
+	$(".listContainer").prepend(html);
 });
 
 $(document).on("click", ".tickIcon", async function () {
-    let id = $(this).parent().data("id");
-    await updateItem(id, true);
-    $(this).parent().addClass("done");
-    $(this).parent().find(".listItemContent").css("justify-content", "flex-start");
-    $(this).parent().find(".tickIcon").remove();
-})
+	let id = $(this).parent().data("id");
+	await updateItem(id, true);
+	$(this).parent().addClass("done");
+	$(this)
+		.parent()
+		.find(".listItemContent")
+		.css("justify-content", "flex-start");
+	$(this).parent().find(".tickIcon").remove();
+});
 
 $(document).on("click", ".delIcon", function () {
-    let id = $(this).parent().data("id");
-    console.log(id)
-    deleteItem(id)
-    $(this).parent().fadeOut();
-})
+	let id = $(this).parent().data("id");
+	deleteItem(id);
+	$(this).parent().fadeOut();
+});
---
+++ serviceworker.js
@@ -0,0 +1,95 @@
+/**
+ * Unlike most Service Workers, this one always attempts to download assets
+ * from the network. Only when network access fails do we fallback to using
+ * the cache. When a request succeeds we always update the cache with the new
+ * version. If a request fails and the result isn't in the cache then we
+ * display an Offline page.
+ */
+const CACHE = "content-v1"; // name of the current cache
+const OFFLINE = "/offline.html"; // URL to offline HTML document
+
+const AUTO_CACHE = [
+	// URLs of assets to immediately cache
+	OFFLINE,
+	"/",
+	"/serviceworker.js",
+	"/manifest.json",
+	"/css/style.css",
+	"/css/sans.ttf",
+	"/apple-touch-icon.png",
+	"/js/jquery.js",
+	"/js/lovefield.js",
+	"/js/lovefield.min.js.map",
+	"/js/script.js",
+	"/favicon.ico",
+	"/logo512.png"
+];
+
+// Iterate AUTO_CACHE and add cache each entry
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		caches
+			.open(CACHE)
+			.then((cache) => cache.addAll(AUTO_CACHE))
+			.then(self.skipWaiting())
+	);
+});
+
+// Destroy inapplicable caches
+self.addEventListener("activate", (event) => {
+	event.waitUntil(
+		caches
+			.keys()
+			.then((cacheNames) => {
+				return cacheNames.filter((cacheName) => CACHE !== cacheName);
+			})
+			.then((unusedCaches) => {
+				console.log("DESTROYING CACHE", unusedCaches.join(","));
+				return Promise.all(
+					unusedCaches.map((unusedCache) => {
+						return caches.delete(unusedCache);
+					})
+				);
+			})
+			.then(() => self.clients.claim())
+	);
+});
+
+self.addEventListener("fetch", (event) => {
+	if (
+		!event.request.url.startsWith(self.location.origin) ||
+		event.request.method !== "GET"
+	) {
+		// External request, or POST, ignore
+		return void event.respondWith(fetch(event.request));
+	}
+
+	event.respondWith(
+		// Always try to download from server first
+		fetch(event.request)
+			.then((response) => {
+				// When a download is successful cache the result
+				caches.open(CACHE).then((cache) => {
+					cache.put(event.request, response);
+				});
+				// And of course display it
+				return response.clone();
+			})
+			.catch((_err) => {
+				// A failure probably means network access issues
+				// See if we have a cached version
+				return caches.match(event.request).then((cachedResponse) => {
+					if (cachedResponse) {
+						// We did have a cached version, display it
+						return cachedResponse;
+					}
+
+					// We did not have a cached version, display offline page
+					return caches.open(CACHE).then((cache) => {
+						const offlineRequest = new Request(OFFLINE);
+						return cache.match(offlineRequest);
+					});
+				});
+			})
+	);
+});
